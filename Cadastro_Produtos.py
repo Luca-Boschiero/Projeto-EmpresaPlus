8f47cd3c915025112ca9a512254e3a8d94bad12a
Luca <luca.bosch.etec@gmail.com>
2026-09-10 12:02:49 -0300

    feat: criação da função de pergunta e importação do colorama

--- Cadastro_Produtos.py
+++ Cadastro_Produtos.py
@@ -1,1 +1,6 @@
-#Dupla B
+from colorama import Fore, Style, init
+init(autoreset=True)
+
+def pergunta(texto_pergunta, cor_pergunta=Fore.CYAN):
+    resposta = input(f"{cor_pergunta}{texto_pergunta}{Style.RESET_ALL} ")
+    return resposta
